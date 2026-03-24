9c660eb5f496eced7fb17431723b896559ef2b3f
Érick Augusto Paludo <erickpaludoyt@gmail.com>
2026-03-24 09:59:22 -0300

    [Security][Feat][1.0.0.0][1] - Desacopla Identity e fortalece autenticação
    - Substitui Identity por implementação própria
    - Adota Argon2 para hash seguro de senhas
    - Implementa ciclo completo de tokens (geração, refresh e revogação)

---
+++ Financ.Infra.Security/Services/Autenticação/AutenticacaoServico.cs
@@ -0,0 +1,105 @@
+﻿using Financ.Application.DTOs.Autenticação.Get;
+using Financ.Application.Interfaces;
+using Financ.Application.Interfaces.Autenticação;
+using Financ.Application.Modelos.Autenticação;
+using Financ.Domain.Entidades;
+using Financ.Infra.Security.Configurações.Autenticação;
+using Financ.Infra.Security.Uteis.Autenticação;
+using Financ.Infra.Security.Uteis.Segurança;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financ.Application.Services.Autenticação
+{
+    public class AutenticacaoServico : IAutenticacaoServico
+    {
+        private readonly AutenticaoConfig _autenticacaoConfig;
+        public AutenticacaoServico(IOptions<AutenticaoConfig> autenticacaoConfig)
+        {
+            _autenticacaoConfig = autenticacaoConfig.Value;
+        }
+        private DateTime GeraExpiracao() => DateTime.UtcNow.AddMinutes(_autenticacaoConfig.ExpiracaoEmMinutos);
+
+        public ResultadoToken GeraToken(string idUsuario, string email)
+        {
+            DateTime expiration = GeraExpiracao();
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            byte[] key = Encoding.UTF8.GetBytes(_autenticacaoConfig.SecretKeyJWT);
+
+            string refreshToken = GeraRefreshToken();
+
+            var claims = new[]
+                {
+                 new Claim(JwtRegisteredClaimNames.Sub, idUsuario),
+                 new Claim(JwtRegisteredClaimNames.Email, email),
+                 new Claim("sid",refreshToken)
+                };
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                claims: claims, 
+                expires: expiration, 
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            );
+
+            var tokenString = handler.WriteToken(token);
+
+            DateTime expirationRefreshToken = DateTime.UtcNow.AddDays(_autenticacaoConfig.ExpitacaoRefreshTokenDias);
+
+            return new ResultadoToken(tokenString,expiration, refreshToken, UtilAutenticacao.DateTimeInUnixTimestamp(expirationRefreshToken), expirationRefreshToken);
+        }
+        public string GeraRefreshToken() => UtilSeguranca.GeraBase64Aleatorios(32);
+        private string CriaAssinatura(byte[] data)
+        {
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(_autenticacaoConfig.SecretKeyJWT));
+            byte[] hash = hmac.ComputeHash(data);
+            return UtilAutenticacao.Base64UrlEncode(hash);
+        }
+        public void ValidaToken(string token)
+        {
+            if (token.Split('.').Count() != 3)
+                throw new Exception("Token inválido.");
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+
+            var jwtExpiration = jwt.Claims.First(x => x.Type.Equals("exp"));
+            var assignature = jwt.RawSignature;
+
+            if (jwtExpiration is null)
+                throw new Exception("Token sem data de expiração.");
+
+            if (UtilAutenticacao.UnixTimestampInDateTime(Convert.ToInt64(jwtExpiration.Value)) < DateTime.UtcNow)
+                throw new Exception("Token expirado.");
+
+            string headerPayload = $"{jwt.RawHeader}.{jwt.RawPayload}";
+
+            if (assignature != CriaAssinatura(Encoding.UTF8.GetBytes(headerPayload)))
+                throw new Exception("Token não autorizado.");
+        }
+        private void ValidaRefresh(string antigoRefreshToken, string novoRefreshToken,long expirationRefresh)
+        {
+            if (!antigoRefreshToken.Equals(novoRefreshToken))
+                throw new Exception("Refresh Token inválido.");
+
+            if (UtilAutenticacao.UnixTimestampInDateTime(expirationRefresh) < DateTime.UtcNow)
+                throw new Exception("Refresh Token expirado.");
+        }
+        public ResultadoToken RefreshToken(Autenticacao autenticacao ,string antigoRefreshToken)
+        {
+            ValidaRefresh(antigoRefreshToken, autenticacao!.RefreshToken!, autenticacao.ExpirationRefresh!.Value);
+
+            return GeraToken(autenticacao.IdUsuario, autenticacao.Usuario.Email);
+        }
+    }
+}
---
+++ Financ.Infra.Security/Services/Segurança/SegurancaServico.cs
@@ -0,0 +1,52 @@
+﻿using Financ.Application.Interfaces;
+using Financ.Application.Interfaces.Segurança;
+using Financ.Infra.Security.Configurações.Segurança;
+using Financ.Infra.Security.Uteis.Segurança;
+using Konscious.Security.Cryptography;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financ.Application.Services.Segurança
+{
+    public class SegurancaServico : ISegurancaServico
+    {
+        private readonly SegurancaConfig _segurancaConfig;
+        public SegurancaServico(IOptions<SegurancaConfig> segurancaConfig)
+        {
+            _segurancaConfig = segurancaConfig.Value;
+        }
+        public (string salt, string hash) CriaPassArgon(string senha,string? salt = null)
+        {
+            byte[] senhaBytes = Encoding.UTF8.GetBytes(senha);
+            byte[] saltBytes = salt is null ? UtilSeguranca.GeraBytesAleatorios(32) : Convert.FromBase64String(salt);
+
+            var cripto = new Argon2id(senhaBytes)
+            {
+                DegreeOfParallelism = 4,
+                MemorySize = 8192,
+                Iterations = 80,
+                Salt = saltBytes,
+                KnownSecret = UtilSeguranca.ConverteParaBytes(_segurancaConfig.Pepper)
+            };
+
+            var hash = cripto.GetBytes(32);
+            string hashBase = Convert.ToBase64String(hash);
+            string saltBase = Convert.ToBase64String(saltBytes);
+
+            return (saltBase, hashBase);
+        }
+
+        public bool ValidaPassArgon(string senhaBanco, string senha,string salt)
+        {
+           return CryptographicOperations.FixedTimeEquals(
+               UtilSeguranca.ConverteParaBytes(senhaBanco),
+               UtilSeguranca.ConverteParaBytes(CriaPassArgon(senha, salt).hash)
+            );
+        } 
+    }
+}
