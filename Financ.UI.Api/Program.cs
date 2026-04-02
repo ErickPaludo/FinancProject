dee7e010fdd7bcad14c9c5173608713fe54b3f2a
Érick Augusto Paludo <erickpaludoyt@gmail.com>
2026-04-02 11:19:44 -0300

    [Api][Feat][2.1.0.1][1] - CORS ajustado para permitir o github pages

--- Financ.UI.Api/Program.cs
+++ Financ.UI.Api/Program.cs
@@ -31,7 +31,7 @@ namespace Financ.UI.Api
                 options.AddPolicy(name: MyAllowSpecificOrigins,
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:5173")
+                        policy.WithOrigins("http://localhost:5173", "https://financ-gestaofinanceira.github.io")
                               .AllowAnyMethod()
                               .AllowAnyHeader()
                               .AllowCredentials();
